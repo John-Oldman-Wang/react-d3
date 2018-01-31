597d7160550a30a2ab08938363727c5e1bc4423c
wanglin <mag1c_shen_09@foxmail.com>
2018-01-31 15:52:36 +0800

    1.1.4 # complete the chinamap component
    1.1.4 # complete the chinamap component

--- src/components/chinaMap.js
+++ src/components/chinaMap.js
@@ -1,13 +1,32 @@
 const React = require("react")
 const { Component } = require("react")
 const ReactDom = require("react-dom")
+
+const d3 = require("d3")
+var color = d3.schemeCategory20;
+color = color.concat(d3.schemeCategory20b);
+color = color.concat(d3.schemeCategory20c);
+var projection, path;
+
+var config = {
+    width: 1200,
+    height: 1000
+}
 class ChinaMap extends Component {
     constructor(props) {
         super(props)
-        this.state={
-            url: ""
+        var root = require('../../json/china.js')
+        // console.log(Object.keys (root) )
+        projection = d3.geoMercator()
+            .center([107, 31])
+            .scale(Math.min(config.width, config.height))
+            .translate([config.width / 2 + 30, config.height / 2 + 0.15 * config.height]);
+        path = d3.geoPath().projection(projection);
+        this.state = {
+            features: root.features,
+            focus: ""
         }
-        window.m=this
+        window.m = this
     }
     componentWillReceiveProps() {
         //console.log("componentWillReceiveProps", arguments)
@@ -26,75 +45,102 @@ class ChinaMap extends Component {
         // console.log("componentWillMount", arguments)
     }
     componentDidMount() {
-        var svg = d3.select(this.refs.svg)
-
-        var projection = d3.geo.mercator()
-            .center([107, 31])
-            .scale(850)
-            .translate([width / 2, height / 2]);
-
-        var path = d3.geo.path()
-            .projection(projection);
-
-
-        var color = d3.scale.category20();
-
-
-        d3.json(this.state.url, function (error, root) {
-
-            if (error)
-                return console.error(error);
-            console.log(root.features);
-
-            svg.selectAll("path")
-                .data(root.features)
-                .enter()
-                .append("path")
-                .attr("stroke", "#000")
-                .attr("stroke-width", 1)
-                .attr("fill", function (d, i) {
-                    return color(i);
-                })
-                .attr("d", path)
-                .on("mouseover", function (d, i) {
-                    d3.select(this)
-                        .attr("fill", "yellow");
-                })
-                .on("mouseout", function (d, i) {
-                    d3.select(this)
-                        .attr("fill", color(i));
-                });
-
-        });
-
         // console.log("componentDidMount", arguments)
     }
     render() {
-        return (
-            <div>
-                <input type='file' onChange={(e)=>{
-                    var read=new FileReader()
-                    read.addEventListener('load',(e)=>{
-                        console.log(e.target.result)
-                        var blob = new Blob([e.target.result])
-                        console.log(blob)
-                        var url=URL.createObjectURL(blob)
-                        this.setState({
-                            url:url
-                        })
-                    })
-                    console.log(e.target.files)
-                    if (e.target.files.length>0){
-                        read.readAsArrayBuffer(e.target.files[0])
-                    }
-                }}/>
-                <svg ref="svg" style={{
-                    width: 1000,
-                    height: 800,
-                    userSelect: "none"
-                }}></svg>
-            </div>
-        )
+        if (this.state.focus == '') {
+            return (
+                <div>
+                    <svg ref="svg" className='map' style={{
+                        width: config.width,
+                        height: config.height,
+                        userSelect: 'none',
+                        transition: 'none',
+                        border: '1px solid red'
+                    }}>
+                        <g>
+                            {this.state.features.map((item, index) => {
+                                return (
+
+                                    <path
+                                        key={index}
+                                        fill={color[index]}
+                                        d={path(item)}
+                                        stroke='#222'
+                                        strokeWidth='1'
+                                        onMouseOver={(e) => {
+                                            var self = e.target
+                                            self.setAttribute('fill', 'yellow')
+                                        }}
+                                        onMouseOut={(e) => {
+                                            var self = e.target
+                                            self.setAttribute('fill', color[index])
+                                        }}
+                                        onClick={(e) => {
+                                            this.setState({
+                                                focus: item
+                                            })
+                                        }}
+                                    ></path>
+                                )
+                            })}
+                        </g>
+                        <g>
+                            {this.state.features.map((item, index) => {
+                                var xy = path.centroid(item)
+                                return (
+                                    <text
+                                        onClick={(e) => {
+                                            this.setState({
+                                                focus: item
+                                            })
+                                        }}
+                                        key={index}
+                                        x={xy[0]}
+                                        y={xy[1]}
+                                        fill="#000"
+                                    >{item.properties.name}
+                                    </text>)
+                            })}
+                        </g>
+                    </svg>
+                </div>
+            )
+        } else {
+            var $projection = d3.geoMercator().center(this.state.focus.properties.cp).scale(this.state.focus.properties.size * 3).translate([config.width / 2, config.height / 2])
+            var $path = d3.geoPath().projection($projection)
+            var axis = $path.centroid(this.state.focus)
+            return (
+                <div>
+                    <svg ref="svg" className='map' style={{
+                        width: config.width,
+                        height: config.height,
+                        userSelect: 'none',
+                        transition: 'none',
+                        border: '1px solid red'
+                    }}>
+                        <g onClick={() => {
+                            this.setState({
+                                focus: ''
+                            })
+                        }}>
+                            <path
+                                stroke='#222'
+                                strokeWidth='1'
+                                fill={color[this.state.focus.properties.childNum]}
+                                d={$path(this.state.focus)}
+                            />
+                            <text
+                                x={axis[0]}
+                                y={axis[1]}
+                                fill="#000"
+                            >{this.state.focus.properties.name}
+                            </text>
+                        </g>
+                    </svg>
+                </div>
+            )
+        }
     }
 }
 module.exports = ChinaMap
